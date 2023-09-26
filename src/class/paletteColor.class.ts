3700577ed134a55c9e37cc9fbc62dcf1d0c28d85
EduAZ86 <eduardoayaviri@gmail.com>
2023-09-26 13:56:40 -0300

    time ago and date create

--- src/class/paletteColor.class.ts
+++ src/class/paletteColor.class.ts
@@ -1,4 +1,3 @@
-import { v4 as uuidv4 } from 'uuid';
 export class ColorPallete {
     idColors:string;
     first_color:string;
@@ -13,13 +12,19 @@ export class ColorPallete {
     fourth_color_rgb:string;
     fifth_color_rgb:string;
 
+    tags:string[] = [''];
+    views:number = 0;
+    shares:number = 0;
+    create_date_ms:number;
+    create_date:string;
+    time_ago:string = '0 seconds';
 
     constructor(
         first_color:string,
         second_color:string,    
         third_color:string,    
         fourth_color:string,
-        fifth_color:string,
+        fifth_color:string,       
     ) {
         this.first_color = first_color
         this.second_color = second_color
@@ -32,6 +37,8 @@ export class ColorPallete {
         this.fourth_color_rgb = this.hexToRgb(this.fourth_color);
         this.fifth_color_rgb = this.hexToRgb(this.fifth_color);
         this.idColors = this.idGenerator(this.first_color, this.second_color, this.third_color, this.fourth_color, this.fifth_color )
+        this.create_date_ms = Date.now()
+        this.create_date = this.stringCreateData(this.create_date_ms)
     }
     private hexToRgb(hex: string): string {
         hex = hex.replace(/^#/, '')
@@ -54,5 +61,59 @@ export class ColorPallete {
         return `${first}-${second}-${third}-${fourth}-${fifth}`
     }
 
+    private stringCreateData (date_ms:number){     
+        const dateString = new Date(date_ms).toLocaleString();
+        return dateString
+    }
+
+
+    public setViews(views:number){
+        this.views = views
+    }
+
+    public setTags(tags:string[]){
+        this.tags = this.tags.concat(tags)
+    }
+    public setTimeAgo() {
+        const createDateMs = this.create_date_ms
+        const second = 1000
+        const minute = second * 60
+        const hour = minute * 60
+        const day = hour * 24
+        const week = day * 7
+        const mounth = day * 31
+        const year = mounth * 12    
+        const current_Date_ms = Date.now();
+        const time_ago_ms = current_Date_ms - createDateMs
+    
+        if (time_ago_ms < minute) {
+            const time_seconds = Math.floor(time_ago_ms/second)
+            this.time_ago = `${time_seconds} seconds`
+        }
+        if (time_ago_ms >= minute && time_ago_ms < hour ) {
+            const time_minutes = Math.floor(time_ago_ms/minute)
+            this.time_ago = `${time_minutes} minutes`
+        }
+        if (time_ago_ms >= hour && time_ago_ms < day ) {
+            const time_hour = Math.floor(time_ago_ms/hour)
+            this.time_ago = `${time_hour} hours`
+        }
+        if (time_ago_ms >= day && time_ago_ms < week ) {
+            const time_day = Math.floor(time_ago_ms/day)
+            this.time_ago = `${time_day} days`
+        }
+        if (time_ago_ms >= week && time_ago_ms < (mounth) ) {
+            const time_week = Math.floor(time_ago_ms/week)
+            this.time_ago = `${time_week} weeks`
+        }
+        if (time_ago_ms >= mounth && time_ago_ms < (year) ) {
+            const time_mounth = Math.floor(time_ago_ms/mounth)
+            this.time_ago = `${time_mounth} mounths`
+        }
+        if (time_ago_ms >= year ) {
+            const time_year = Math.floor(time_ago_ms/year)
+            this.time_ago = `${time_year} years`
+        }    
+    }
 
 }
